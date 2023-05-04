488efebee57311513c9d72159bf15a84bf621897
ABHIRUP <abhirupsaha02@gmail.com>
2023-05-04 13:00:43 +0530

    some changes

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const videoFileMap = {
     'valorant': 'videos/Valorant.mp4'
 }
 
-app.get('/videos/:videoname', (req, res) => {
+app.get('/videos/:videoname', async (req, res) => {
     const fileName = req.params.videoname
     const filePath = videoFileMap[fileName]
     if (!filePath) {
@@ -21,21 +21,18 @@ app.get('/videos/:videoname', (req, res) => {
     const range = req.headers.range
 
     if (range) {
-        const parts = range.replace(/bytes=/, '').split('-')
-        const start = parseInt(parts[0], 10)
-        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
+        let [start, end] = range.replace(/bytes=/, '').split('-')
+        start = parseInt(parts[0], 10)
+        end = end ? parseInt(parts[1], 10) : fileSize - 1
 
-        const chunkSize = end - start + 1
-        const fileStream = fs.createReadStream(filePath, { start, end })
-
-        const head = {
+        
+        res.writeHead(206, {
             'Content-Range': `bytes ${start}-${end}/${fileSize}`,
             'Accept-ranges': 'bytes',
-            'Content-Length': chunkSize,
+            'Content-Length': end -start + 1,
             'Content-Type': 'video/mp4'
-        }
-        res.writeHead(206, head)
-        fileStream.pipe(res)
+        })
+        fs.createReadStream(filePath, { start, end }).pipe(res)
     }
     else {
         const head = {
